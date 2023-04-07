1116dcb91db174a95367ef08ca881a227d724251
shkarin <shkarin@protei.ru>
2023-04-07 17:21:58 +0300

    Added tests

---
+++ tests/EsApi.Tests/Types/ExtensionsTest.cs
@@ -0,0 +1,62 @@
+﻿using System.Reflection;
+using AutoFixture;
+using ServerLogic.Types;
+using Xunit;
+
+namespace ServerLogic.Tests.Types
+{
+    public class ExtensionsTest
+    {
+        private readonly IFixture _builder;
+
+        public ExtensionsTest()
+        {
+            _builder = new Fixture();
+        }
+
+        [Fact]
+        public void NullPropertiesMeansEqualForIsMappingTo()
+        {
+            var cardConditionFirst = _builder.Build<CardCondition>()
+                                             .With(x => x.IsIncident, true)
+                                             .Without(x => x.IsComplex)
+                                             .Without(x => x.IsPriority)
+                                             .Without(x => x.IsTest)
+                                             .Without(x => x.IsControl)
+                                             .Create();
+
+            var cardConditionSecond = _builder.Build<CardCondition>()
+                                              .With(x => x.IsIncident, true)
+                                              .Create();
+
+            Assert.True(cardConditionFirst.IsMappingTo(cardConditionSecond));
+        }
+
+        [Fact]
+        public void EqualsCardConditionsMappingAndNotEqualAreNot()
+        {
+            var cardConditionFirst = _builder.Create<CardCondition>();
+
+            var cardConditionSecond = _builder.Create<CardCondition>();
+
+            if (cardConditionFirst.Equals(cardConditionSecond))
+            {
+                Assert.True(cardConditionFirst.IsMappingTo(cardConditionSecond));
+            }
+            else
+            {
+                Assert.False(cardConditionFirst.IsMappingTo(cardConditionSecond));
+            }
+        }
+
+        [Fact]
+        public void CountConditionsUsesAllPublicInstanceFields()
+        {
+            var cardConditionFirst = _builder.Create<CardCondition>();
+
+            var publicInstanceFieldsCount = cardConditionFirst.GetType().GetFields(BindingFlags.Public | BindingFlags.Instance).Length;
+
+            Assert.Equal(publicInstanceFieldsCount, cardConditionFirst.CountConditions());
+        }
+    }
+}
---
+++ tests/EsApi.Tests/Types/MapperConfigurationTest.cs
@@ -0,0 +1,34 @@
+﻿using AutoFixture;
+using Dt.EsApi.Mapper;
+using Dt.EsApi.Models;
+using ServerLogic.Types;
+using Xunit;
+
+namespace ServerLogic.Tests.Types
+{
+    public class MapperConfigurationTest
+    {
+        [Fact]
+        public void AllFieldAreMappingTest()
+        {
+            var config = AutoMapperConfigurationFactory.ConfigureMapper();
+            config.AssertConfigurationIsValid();
+        }
+
+        [Fact]
+        public void Card04MappingTest()
+        {
+            // Arrange
+            var mapper = MapperFabric.CreateMapper();
+            var fixture = new Fixture();
+            var cardDto = fixture.Create<Tm_Card_04_Data>();
+
+            // Act
+            var cardModel = mapper.Map<Tm_Card_04_Data, Card04Model>(cardDto);
+            var cardNewDto = mapper.Map<Card04Model, Tm_Card_04_Data>(cardModel);
+
+            // Assert
+            Assert.Equal(cardDto, cardNewDto);
+        }
+    }
+}
